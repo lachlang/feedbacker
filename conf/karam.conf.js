8b93471027aa03b2408f4d956545042751f3874e
Lachlan Gartside <lachlan.gartside@cba.com.au>
2017-06-08 11:35:05 +1000

    scaled back automated testing in config

--- conf/karam.conf.js
+++ conf/karam.conf.js
@@ -65,7 +65,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome','Safari','Firefox'],
+    browsers: ['Chrome'],//,'Safari','Firefox'],
 
 
     // Continuous Integration mode
