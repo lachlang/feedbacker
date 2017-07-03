f806f896015278239ee30dc0c25ed5c6ddeab924
Lachlan Gartside <lachlan.gartside@cba.com.au>
2017-07-03 10:39:24 +1000

    fixed update feedback caching issue

--- app/au/com/feedbacker/controllers/Feedback.scala
+++ app/au/com/feedbacker/controllers/Feedback.scala
@@ -39,8 +39,12 @@ class Feedback @Inject() (person: PersonDao,
         val questionsJson: JsResult[Seq[QuestionResponse]] = json.validate[Seq[QuestionResponse]]((JsPath \ "body" \ "questions").read[Seq[QuestionResponse]])
         (questionsJson, submittedJson, shareFeedbackJson) match {
           case (JsSuccess(questions,_), JsSuccess(submitted,_), JsSuccess(shareFeedback,_)) =>
-            if (nomination.submitFeedback(nominationId, questions, submitted, feedbackCycle.getSharingSettingsForNomination(nominationId,shareFeedback)))
-              Ok(Json.obj("apiVersion" -> "1.0", "body" -> Json.toJson(DetailItem.detailFromNomination(n, feedbackCycle.findById(n.cycleId)))))
+            if (nomination.submitFeedback(nominationId, questions, submitted, feedbackCycle.getSharingSettingsForNomination(nominationId,shareFeedback))) {
+              nomination.getDetail(nominationId) match {
+                case None => BadRequest
+                case Some(updatedNomination) => Ok(Json.obj("apiVersion" -> "1.0", "body" -> Json.toJson(DetailItem.detailFromNomination(updatedNomination, feedbackCycle.findById(n.cycleId)))))
+              }
+            }
             else BadRequest
           case _ => BadRequest
         }
